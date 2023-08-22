26a8f7fb65762699e675ea3be31920937ae1325d
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2023-08-22 14:55:27 -0600

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <YAKL.h>
+#include <YAKL_netcdf.h>
 #include <mpi.h>
 
 int main(int argc, char* argv[]) {
   MPI_Init(&argc, &argv);
   yakl::init();
+  yakl::SimpleNetCDF nc;
   std::cout << "Hello actions" << std::endl;
   yakl::finalize();
   MPI_Finalize();
