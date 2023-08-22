449ed76836203896c1298b13ad23186121e96521
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2023-08-22 14:47:30 -0600

    Add cmakescript

---
+++ build/cmakescript.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+
+cmake      \
+  -DCMAKE_CXX_COMPILER=${CXX}                    \
+  -DCMAKE_CUDA_HOST_COMPILER=${CXX}              \
+  -DYAKL_CUDA_FLAGS="${YAKL_CUDA_FLAGS}"         \
+  -DYAKL_CXX_FLAGS="${YAKL_CXX_FLAGS}"           \
+  -DYAKL_SYCL_FLAGS="${YAKL_SYCL_FLAGS}"         \
+  -DYAKL_OPENMP_FLAGS="${YAKL_OPENMP_FLAGS}"     \
+  -DYAKL_HIP_FLAGS="${YAKL_HIP_FLAGS}"           \
+  -DYAKL_F90_FLAGS="${YAKL_F90_FLAGS}"           \
+  -DMY_LINK_FLAGS="${MY_LINK_FLAGS}"             \
+  -DYAKL_ARCH="${YAKL_ARCH}"                     \
+  ..
