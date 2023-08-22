f2e777439e7613521aa633273070f7d15124accd
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2023-08-22 14:04:47 -0600

    MPI

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <YAKL.h>
+#include <mpi.h>
 
-int main() {
+int main(int argc, char* argv[]) {
+  MPI_Init(&argc, &argv);
   yakl::init();
   std::cout << "Hello actions" << std::endl;
   yakl::finalize();
+  MPI_Finalize();
 }
