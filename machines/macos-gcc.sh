9fcb32f1a973ea477ec82ee9a6b032554ce51576
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2023-08-23 11:23:33 -0600

    wip

--- machines/macos-gcc.sh
+++ machines/macos-gcc.sh
@@ -10,6 +10,10 @@ export CC=mpicc
 export CXX=mpic++
 export FC=mpifort
 
+export OMPI_CC=gcc-12
+export OMPI_CXX=g++-12
+export OMPI_FC=gfortran-12
+
 export YAKL_CXX_FLAGS="-DYAKL_PROFILE -DHAVE_MPI -O2 -I`nc-config --includedir`"
 export YAKL_F90_FLAGS="-O2 -DSCREAM_DOUBLE_PRECISION -ffree-line-length-none"
 export MY_LINK_FLAGS="-L`nc-config --libdir` -lnetcdf"
