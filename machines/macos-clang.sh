b1528980cfa7530f1a27ee82a784a5852429ed7d
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2023-08-23 11:43:04 -0600

    wip

--- machines/macos-clang.sh
+++ machines/macos-clang.sh
@@ -12,7 +12,7 @@ export FC=mpifort
 
 export OMPI_CC=clang
 export OMPI_CXX=clang++
-export OMPI_FC=gfortran
+export OMPI_FC=gfortran-12
 
 export YAKL_CXX_FLAGS="-DYAKL_PROFILE -DHAVE_MPI -O2 -I`nc-config --includedir`"
 export YAKL_F90_FLAGS="-O2 -DSCREAM_DOUBLE_PRECISION -ffree-line-length-none"
